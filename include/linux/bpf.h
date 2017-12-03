52752f3681d25e80bf14a2d1cf5d0af5e0749733
Al Viro <viro@zeniv.linux.org.uk>
2017-12-02 20:20:38 -0500

    BACKPORT: fix "netfilter: xt_bpf: Fix XT_BPF_MODE_FD_PINNED mode of 'xt_bpf_info_v1'"
    Descriptor table is a shared object; it's not a place where you can
    stick temporary references to files, especially when we don't need
    an opened file at all.
    
    Cc: stable@vger.kernel.org # v4.14
    Fixes: 98589a0998b8 ("netfilter: xt_bpf: Fix XT_BPF_MODE_FD_PINNED mode of 'xt_bpf_info_v1'")
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    
    Removed the code related to function bpf_prog_get_ok() since it is not
    exsit in current android tree.
    (cherry picked from commit 040ee69226f8a96b7943645d68f41d5d44b5ff7d)
    
    Change-Id: If7a602128cdea4b4b50c8effb215c9bca7449515
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -308,6 +308,8 @@ static inline void bpf_long_memcpy(void *dst, const void *src, u32 size)
 /* verify correctness of eBPF program */
 int bpf_check(struct bpf_prog **fp, union bpf_attr *attr);
 
+struct bpf_prog *bpf_prog_get_type_path(const char *name, enum bpf_prog_type type);
+
 static inline bool unprivileged_ebpf_enabled(void)
 {
 	return !sysctl_unprivileged_bpf_disabled;
@@ -349,6 +351,12 @@ static inline void __bpf_prog_uncharge(struct user_struct *user, u32 pages)
 static inline int bpf_obj_get_user(const char __user *pathname)
 {
 	return -EOPNOTSUPP;
+}
+
+static inline struct bpf_prog *bpf_prog_get_type_path(const char *name,
+				enum bpf_prog_type type)
+{
+	return ERR_PTR(-EOPNOTSUPP);
 }
 
 static inline bool unprivileged_ebpf_enabled(void)
